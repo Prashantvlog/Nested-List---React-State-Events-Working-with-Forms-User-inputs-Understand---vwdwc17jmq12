79bf510d16317abce453b5f50967218f4bf8c221
Prashant Sonwane <prashants0209@gmail.com>
2023-03-06 20:13:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,161 +1,96 @@
-import React, { Component, useState } from "react";
-import "./../styles/App.css";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-// Do not alter the states const and values inside it.
-const states = [
-  {
-    name: "Madhya Pradesh",
-    cities: [
-      {
-        name: "Indore",
-        towns: [
-          {
-            name: "Mhow",
-          },
-          {
-            name: "Dewas",
-          },
-        ],
-      },
-      {
-        name: "Bhopal",
-        towns: [
-          {
-            name: "Manit",
-          },
-          {
-            name: "Berasia",
-          },
-        ],
-      },
-      {
-        name: "Gwalior",
-        towns: [
-          {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Jharkhand",
-    cities: [
-      {
-        name: "Dhanbad",
-        towns: [
-          {
-            name: "IIT(ISM) Dhanbad",
-          },
-          {
-            name: "Hirapur",
-          },
-        ],
-      },
-      {
-        name: "Wasseypur",
-        towns: [
-          {
-            name: "Sardar khan's",
-          },
-          {
-            name: "Faizal khan's",
-          },
-        ],
-      },
-      {
-        name: "Mirzapur",
-        towns: [
-          {
-            name: "Kaleen bhaiya's",
-          },
-          {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Assam",
-    cities: [
-      {
-        name: "Guwhati",
-        towns: [
-          {
-            name: "Amin",
-          },
-          {
-            name: "Jalah",
-          },
-        ],
-      },
-      {
-        name: "Jungle1",
-        towns: [
-          {
-            name: "Tiger found at IIT Guwahati",
-          },
-          {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
-      },
-      {
-        name: "Tezpur",
-        towns: [
-          {
-            name: "Dibrugarh",
-          },
-          {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Bihar",
-    cities: [
-      {
-        name: "Patna",
-        towns: [
-          {
-            name: "Sonpur",
-          },
-          {
-            name: "Maner",
-          },
-        ],
-      },
-      {
-        name: "Gaya",
-        towns: [
-          {
-            name: "Bakraur",
-          },
-          {
-            name: "Barachatti",
-          },
-        ],
-      },
-      {
-        name: "Darbhanga",
-        towns: [
-          {
-            name: "Singhwara",
-          },
-          {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
-];
 
-function App() {
-  return <div id="main"></div>;
+const App = () => {
+
+    const validate = (event) => {
+      
+    event.preventDefault();
+    var email = document.querySelector("input[data-testid='email']").value;
+    const error = document.getElementById("error");
+    const welcome = document.getElementById("welcome");
+    var input = document.querySelectorAll("input");
+    var name = input[0].value;
+    var password = input[3].value;
+    
+
+      for(var i=0 ; i<input.length ; i++){
+
+        console.log(input[i].value.length);
+
+        if(input[i].value.length == 0){
+
+            error.innerHTML = "<h1>All fields are mandatory</h1>";
+                return;
+        }
+
+      }
+
+      var matches = name.match(/(\d+)/);
+      console.log(matches);
+
+      if(matches == null){
+
+        error.innerHTML = "<h1>Name is not alphanumeric</h1>";
+        return;
+      }
+
+      if( email.indexOf('@') == -1)
+      {
+        welcome.innerHTML = "";
+        error.innerHTML = "<h1>Email must contain @</h1>";
+
+        return
+      }
+
+      if(password.length <6){
+        error.innerHTML = "<h1>Password must contain atleast 6 letters</h1>";
+        return;
+      }
+      
+      console.log("email="+email);
+
+      error.innerHTML ="";
+
+      welcome.innerHTML = "<h1>Hello "+email.slice(0,email.indexOf('@'))+"</h1>";
+
+    }
+
+  
+
+  return (
+
+    <div id="main">
+
+      <form  onSubmit={validate}>
+
+        <label>name</label>
+        <input type="text" data-testid = 'name'  />
+        <label>Email</label>
+        <input type="text" data-testid='email' id = "email"  /> 
+        <label>Gender</label>
+
+        <select data-testid='gender' placeholder="Gender">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Others</option>
+        </select>
+
+          <label>Phone Number</label>
+          <input type="text" data-testid='phoneNumber'   />
+          <label>Password</label>
+          <input type="password" data-testid = "password"  />
+          <button type="submit" data-testid="submit">Submit</button>
+
+        </form>
+
+        <div id="error"></div>
+        <div id="welcome"></div>
+    </div>
+
+  )
 }
+
 
 export default App;
